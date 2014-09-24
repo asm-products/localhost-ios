08943a8aa7e88336c9a0a8745f0f9dad7f3d89ef
Michael Basil <michael@intrications.com>
2014-09-24 22:06:20 +0100

    Create basic iOS project - Bounty #18

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, "6.0"
+
+target "localhost" do
+pod 'Facebook-iOS-SDK', '~> 3.18'
+end
+
+target "localhostTests" do
+
+end
+
---
+++ localhost/LoginViewController.swift
@@ -0,0 +1,49 @@
+//
+//  LoginViewController.swift
+//  localhost
+//
+//  Created by Michael Basil on 24/09/2014.
+//  Copyright (c) 2014 Localhost. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController, FBLoginViewDelegate {
+
+    @IBOutlet weak var fbLoginView: FBLoginView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.fbLoginView.delegate = self
+        self.fbLoginView.readPermissions = ["public_profile", "email", "user_friends"]
+    }
+    
+    func loginViewShowingLoggedInUser(loginView : FBLoginView!) {
+        println("User Logged In")
+    }
+    
+    func loginViewFetchedUserInfo(loginView : FBLoginView!, user: FBGraphUser) {
+        println("User: \(user)")
+        println("User ID: \(user.objectID)")
+        println("User Name: \(user.name)")
+        var userEmail = user.objectForKey("email") as String
+        println("User Email: \(userEmail)")
+    }
+    
+    func loginViewShowingLoggedOutUser(loginView : FBLoginView!) {
+        println("User Logged Out")
+    }
+    
+    func loginView(loginView : FBLoginView!, handleError:NSError) {
+        println("Error: \(handleError.localizedDescription)")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
